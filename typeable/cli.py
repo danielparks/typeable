29d3189c10340db5bc6d28e1539a222f57b300e2
Daniel Parks <dp+git@oxidized.org>
2017-04-10 00:23:37 -0700

    Add filter command

--- typeable/cli.py
+++ typeable/cli.py
@@ -2,16 +2,35 @@ import click
 import sys
 import typeable.runs
 
-@click.command()
-@click.option('--string', '-s', 'strings', multiple=True)
-@click.version_option()
-def main(strings):
+def querty():
     qwerty = [
         "`~12345~!@#$%123456qwertasdfgzxcvb\t",
         "67890-=^&*()_+yuiop[]\\{}|hjkl;':\"nm,./<>?"]
-    qwerty = [set(chars.lower() + chars.upper()) for chars in qwerty]
+    return [set(chars.lower() + chars.upper()) for chars in qwerty]
 
+@click.group()
+@click.version_option()
+def main():
+    pass
+
+@main.command()
+@click.option('--string', '-s', 'strings', multiple=True)
+def run_percent(strings):
+    hands = querty()
     for string in strings:
-        print("{:3.0f}% {}".format(
-            typeable.runs.fraction(string, qwerty)*100,
+        click.echo("{:3.0f}% {}".format(
+            typeable.runs.fraction(string, hands)*100,
             string))
+
+@main.command()
+@click.argument('files', nargs=-1, type=click.File('r'))
+@click.option('--maximum-run-percent', '-m', type=click.IntRange(0, 100), default=50)
+def filter(files, maximum_run_percent=0.5):
+    if not files:
+        files = [sys.stdin]
+    hands = querty()
+    for file in files:
+        for line in file:
+            line = line.rstrip("\n")
+            if typeable.runs.fraction(line, hands)*100 <= maximum_run_percent:
+                click.echo(line)
