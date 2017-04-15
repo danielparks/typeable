77e70712958628455abaae2f7d8895bce3cf3564
Daniel Parks <dp+git@oxidized.org>
2017-04-15 16:41:43 -0700

    run_count command

--- typeable/cli.py
+++ typeable/cli.py
@@ -23,6 +23,15 @@ def run_percent(strings):
             string))
 
 @main.command()
+@click.option('--string', '-s', 'strings', multiple=True)
+def run_count(strings):
+    hands = querty()
+    for string in strings:
+        click.echo("{:2d} {}".format(
+            typeable.runs.count(string, hands),
+            string))
+
+@main.command()
 @click.argument('files', nargs=-1, type=click.File('r'))
 @click.option('--maximum-run-percent', '-m', type=click.IntRange(0, 100), default=50)
 def filter(files, maximum_run_percent=0.5):
